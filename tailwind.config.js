dee65db57641ccc5a2d53166a765c654c965a4e4
alecdalelio <alecdalelio@gmail.com>
2021-11-28 19:30:39 -0500

    chore: use tailwind config from nextjs-advanced-starter template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [],
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  // eslint-disable-next-line global-require
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
